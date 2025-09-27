01045cac571a330f35cfda058fe643c219eea40a
aaronlitvack-tmu <aaron.litvack@torontomu.ca>
2025-09-27 14:43:34 -0400

    Included up to step 3; correlation analysis

--- AER850_Proejct1_F2025.py
+++ AER850_Proejct1_F2025.py
@@ -1,1 +1,23 @@
-print("Hello World")
+#Step 1: Data Processing
+import pandas as pd
+data = pd.read_csv("data/Project 1 Data.csv")
+
+#Step 2: Data Visualization
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.plot(data['Step'], data['X'], label='X')
+plt.plot(data['Step'], data['Y'], label='Y')
+plt.plot(data['Step'], data['Z'], label='Z')
+plt.xlabel('Step')
+plt.ylabel('XYZ Coordinate')
+plt.title('XYZ Coordiantes')
+plt.legend(loc='best')
+plt.show()
+
+#Step 3: Correlation Analysis
+import seaborn as sns
+
+corr_matrix = data.corr()
+sns.heatmap((corr_matrix))
+
